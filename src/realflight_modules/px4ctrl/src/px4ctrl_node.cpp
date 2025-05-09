a7b08ab747a56e86ae4d2504f283c633cf704f28
Ultramarine1939-syujie <2021218017@mail.hfut.edu.cn>
2025-05-09 16:39:04 +0800

    更新了适用于虚拟环境的参数，删除修改了不必要的组件

--- src/realflight_modules/px4ctrl/src/px4ctrl_node.cpp
+++ src/realflight_modules/px4ctrl/src/px4ctrl_node.cpp
@@ -1,86 +1,51 @@
-#include <ros/ros.h>
-#include "PX4CtrlFSM.h"
-#include <signal.h>
+#include <ros/ros.h>  // 包含ROS的头文件
+#include "PX4CtrlFSM.h"  // 包含自定义的PX4CtrlFSM类的头文件
+#include <signal.h>  // 包含处理信号的头文件
 
 void mySigintHandler(int sig)
 {
-    ROS_INFO("[PX4Ctrl] exit...");
-    ros::shutdown();
+    ROS_INFO("[PX4Ctrl] exit...");  // 打印信息到ROS日志
+    ros::shutdown();  // 关闭ROS节点
 }
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "px4ctrl");
-    ros::NodeHandle nh("~");
+    ros::init(argc, argv, "px4ctrl");  // 初始化ROS节点
+    ros::NodeHandle nh("~");  // 创建ROS节点句柄
 
-    signal(SIGINT, mySigintHandler);
-    ros::Duration(1.0).sleep();
+    signal(SIGINT, mySigintHandler);  // 注册SIGINT信号的处理函数
+    ros::Duration(1.0).sleep();  // 等待1秒钟
 
     Parameter_t param;
-    param.config_from_ros_handle(nh);
+    param.config_from_ros_handle(nh);  // 从ROS参数服务器中获取参数配置
 
-    // Controller controller(param);
-    LinearControl controller(param);
-    PX4CtrlFSM fsm(param, controller);
+    LinearControl controller(param);  // 创建LinearControl对象
+    PX4CtrlFSM fsm(param, controller);  // 创建PX4CtrlFSM状态机对象
 
-    ros::Subscriber state_sub =
-        nh.subscribe<mavros_msgs::State>("/mavros/state",
-                                         10,
-                                         boost::bind(&State_Data_t::feed, &fsm.state_data, _1));
-
-    ros::Subscriber extended_state_sub =
-        nh.subscribe<mavros_msgs::ExtendedState>("/mavros/extended_state",
-                                                 10,
-                                                 boost::bind(&ExtendedState_Data_t::feed, &fsm.extended_state_data, _1));
-
-    ros::Subscriber odom_sub =
-        nh.subscribe<nav_msgs::Odometry>("odom",
-                                         100,
-                                         boost::bind(&Odom_Data_t::feed, &fsm.odom_data, _1),
-                                         ros::VoidConstPtr(),
-                                         ros::TransportHints().tcpNoDelay());
-
-    ros::Subscriber cmd_sub =
-        nh.subscribe<quadrotor_msgs::PositionCommand>("cmd",
-                                                      100,
-                                                      boost::bind(&Command_Data_t::feed, &fsm.cmd_data, _1),
-                                                      ros::VoidConstPtr(),
-                                                      ros::TransportHints().tcpNoDelay());
-
-    ros::Subscriber imu_sub =
-        nh.subscribe<sensor_msgs::Imu>("/mavros/imu/data", // Note: do NOT change it to /mavros/imu/data_raw !!!
-                                       100,
-                                       boost::bind(&Imu_Data_t::feed, &fsm.imu_data, _1),
-                                       ros::VoidConstPtr(),
-                                       ros::TransportHints().tcpNoDelay());
-
+    // 订阅各种传感器数据和控制命令
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("/mavros/state", 10, boost::bind(&State_Data_t::feed, &fsm.state_data, _1));
+    ros::Subscriber extended_state_sub = nh.subscribe<mavros_msgs::ExtendedState>("/mavros/extended_state", 10, boost::bind(&ExtendedState_Data_t::feed, &fsm.extended_state_data, _1));
+    ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>("odom", 100, boost::bind(&Odom_Data_t::feed, &fsm.odom_data, _1), ros::VoidConstPtr(), ros::TransportHints().tcpNoDelay());
+    ros::Subscriber cmd_sub = nh.subscribe<quadrotor_msgs::PositionCommand>("cmd", 100, boost::bind(&Command_Data_t::feed, &fsm.cmd_data, _1), ros::VoidConstPtr(), ros::TransportHints().tcpNoDelay());
+    ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("/mavros/imu/data", 100, boost::bind(&Imu_Data_t::feed, &fsm.imu_data, _1), ros::VoidConstPtr(), ros::TransportHints().tcpNoDelay());
+    
+    // 根据是否连接遥控器订阅遥控器数据
     ros::Subscriber rc_sub;
-    if (!param.takeoff_land.no_RC) // mavros will still publish wrong rc messages although no RC is connected
+    if (!param.takeoff_land.no_RC)
     {
-        rc_sub = nh.subscribe<mavros_msgs::RCIn>("/mavros/rc/in",
-                                                 10,
-                                                 boost::bind(&RC_Data_t::feed, &fsm.rc_data, _1));
+        rc_sub = nh.subscribe<mavros_msgs::RCIn>("/mavros/rc/in", 10, boost::bind(&RC_Data_t::feed, &fsm.rc_data, _1));
     }
+    
+    // 订阅电池状态和起飞降落指令
+    ros::Subscriber bat_sub = nh.subscribe<sensor_msgs::BatteryState>("/mavros/battery", 100, boost::bind(&Battery_Data_t::feed, &fsm.bat_data, _1), ros::VoidConstPtr(), ros::TransportHints().tcpNoDelay());
+    ros::Subscriber takeoff_land_sub = nh.subscribe<quadrotor_msgs::TakeoffLand>("takeoff_land", 100, boost::bind(&Takeoff_Land_Data_t::feed, &fsm.takeoff_land_data, _1), ros::VoidConstPtr(), ros::TransportHints().tcpNoDelay());
 
-    ros::Subscriber bat_sub =
-        nh.subscribe<sensor_msgs::BatteryState>("/mavros/battery",
-                                                100,
-                                                boost::bind(&Battery_Data_t::feed, &fsm.bat_data, _1),
-                                                ros::VoidConstPtr(),
-                                                ros::TransportHints().tcpNoDelay());
-
-    ros::Subscriber takeoff_land_sub =
-        nh.subscribe<quadrotor_msgs::TakeoffLand>("takeoff_land",
-                                                  100,
-                                                  boost::bind(&Takeoff_Land_Data_t::feed, &fsm.takeoff_land_data, _1),
-                                                  ros::VoidConstPtr(),
-                                                  ros::TransportHints().tcpNoDelay());
-
+    // 发布控制命令和调试信息
     fsm.ctrl_FCU_pub = nh.advertise<mavros_msgs::AttitudeTarget>("/mavros/setpoint_raw/attitude", 10);
     fsm.traj_start_trigger_pub = nh.advertise<geometry_msgs::PoseStamped>("/traj_start_trigger", 10);
+    fsm.debug_pub = nh.advertise<quadrotor_msgs::Px4ctrlDebug>("/debugPx4ctrl", 10);  // 调试信息
 
-    fsm.debug_pub = nh.advertise<quadrotor_msgs::Px4ctrlDebug>("/debugPx4ctrl", 10); // debug
-
+    // 创建服务客户端用于设置FCU模式、解锁和重启飞控
     fsm.set_FCU_mode_srv = nh.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
     fsm.arming_client_srv = nh.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
     fsm.reboot_FCU_srv = nh.serviceClient<mavros_msgs::CommandLong>("/mavros/cmd/command");
@@ -89,38 +54,39 @@ int main(int argc, char *argv[])
 
     if (param.takeoff_land.no_RC)
     {
-        ROS_WARN("PX4CTRL] Remote controller disabled, be careful!");
+        ROS_WARN("[PX4CTRL] Remote controller disabled, be careful!");  // 打印警告信息
     }
     else
     {
-        ROS_INFO("PX4CTRL] Waiting for RC");
+        ROS_INFO("[PX4CTRL] Waiting for RC");  // 打印信息
         while (ros::ok())
         {
             ros::spinOnce();
             if (fsm.rc_is_received(ros::Time::now()))
             {
-                ROS_INFO("[PX4CTRL] RC received.");
+                ROS_INFO("[PX4CTRL] RC received.");  // 打印信息
                 break;
             }
             ros::Duration(0.1).sleep();
         }
     }
 
+    // 等待连接到PX4飞控
     int trials = 0;
     while (ros::ok() && !fsm.state_data.current_state.connected)
     {
         ros::spinOnce();
         ros::Duration(1.0).sleep();
         if (trials++ > 5)
-            ROS_ERROR("Unable to connnect to PX4!!!");
+            ROS_ERROR("Unable to connect to PX4!!!");  // 打印错误信息
     }
 
-    ros::Rate r(param.ctrl_freq_max);
+    ros::Rate r(param.ctrl_freq_max);  // 设置控制频率
     while (ros::ok())
     {
         r.sleep();
         ros::spinOnce();
-        fsm.process(); // We DO NOT rely on feedback as trigger, since there is no significant performance difference through our test.
+        fsm.process();  // 处理状态机逻辑
     }
 
     return 0;
--- src/realflight_modules/px4ctrl/thrust_calibrate_scrips/thrust_calibrate.py
+++ src/realflight_modules/px4ctrl/thrust_calibrate_scrips/thrust_calibrate.py
@@ -10,111 +10,95 @@ from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import BatteryState
 from mavros_msgs.msg import AttitudeTarget
 
-
 class ThrustCalibration:
 
     def __init__(self):
 
-        # Load parameters
-        self.time_average_interval = float(
-            rospy.get_param('~time_interval', 1.0))
-        self.vbat_min = float(
-            rospy.get_param('~min_battery_voltage', 13.2))
-        self.mass_kg = float(
-            rospy.get_param('~mass_kg', 1.0))
+        # 加载参数
+        self.time_average_interval = float(rospy.get_param('~time_interval', 1.0))  # 时间间隔
+        self.vbat_min = float(rospy.get_param('~min_battery_voltage', 13.2))  # 最小电池电压
+        self.mass_kg = float(rospy.get_param('~mass_kg', 1.0))  # 质量
 
-        # store measurements of vbat and commanded thrusts
-        self.volt_buf = np.array([])
-        self.volt_records = np.array([])
-        self.cmd_buf = np.array([])
-        self.cmd_records = np.array([])
-        self.last_records_t = rospy.get_rostime()
+        # 存储电池电压和推力指令的测量值
+        self.volt_buf = np.array([])  # 电池电压缓存
+        self.volt_records = np.array([])  # 电池电压记录
+        self.cmd_buf = np.array([])  # 推力指令缓存
+        self.cmd_records = np.array([])  # 推力指令记录
+        self.last_records_t = rospy.get_rostime()  # 上次记录时间
 
-        # Some flags
-        self.count_start_trigger_received = 0
+        # 一些标志
+        self.count_start_trigger_received = 0  # 触发信号接收计数
 
-        # Subscribe
-        self.bat_sub = rospy.Subscriber(
-            "/mavros/battery", BatteryState, self.battery_voltage_cb)
-        rospy.Subscriber(
-            "/mavros/setpoint_raw/attitude", AttitudeTarget, self.thrust_commands_cb)
-        rospy.Subscriber(
-            "/traj_start_trigger", PoseStamped, self.start_trigger_cb)
+        # 订阅
+        self.bat_sub = rospy.Subscriber("/mavros/battery", BatteryState, self.battery_voltage_cb)  # 订阅电池状态信息
+        rospy.Subscriber("/mavros/setpoint_raw/attitude", AttitudeTarget, self.thrust_commands_cb)  # 订阅姿态控制指令
+        rospy.Subscriber("/traj_start_trigger", PoseStamped, self.start_trigger_cb)  # 订阅起始触发信号
 
+    # 处理电池电压信息的回调函数
     def battery_voltage_cb(self, msg):
 
-        if (self.count_start_trigger_received == 0):
+        if (self.count_start_trigger_received == 0):  # 如果还未接收到触发信号，退出
             return
 
-        self.volt_buf = np.append(
-            self.volt_buf, np.array([msg.voltage]), axis=0)
+        self.volt_buf = np.append(self.volt_buf, np.array([msg.voltage]), axis=0)  # 将电池电压信息加入缓存
 
-        # Record filted data
+        # 记录滤波后的数据
         cur_t = rospy.get_rostime()
         if ((cur_t - self.last_records_t).to_sec() > self.time_average_interval):
             self.last_records_t = cur_t
-            # print self.volt_records.shape
-            # print self.volt_buf.shape
-            # print
-            # print np.mean(self.volt_buf)
-            self.volt_records = np.append(
-                self.volt_records, np.array([np.mean(self.volt_buf)]), axis=0)
-            self.volt_buf = np.array([])
-            self.cmd_records = np.append(
-                self.cmd_records, np.array([np.mean(self.cmd_buf)]), axis=0)
-            self.cmd_buf = np.array([])
-            print "volt=", self.volt_records[-1], " thr=", self.cmd_records[-1]
+            self.volt_records = np.append(self.volt_records, np.array([np.mean(self.volt_buf)]), axis=0)  # 记录平均电压
+            self.volt_buf = np.array([])  # 清空电压缓存
+            self.cmd_records = np.append(self.cmd_records, np.array([np.mean(self.cmd_buf)]), axis=0)  # 记录平均推力指令
+            self.cmd_buf = np.array([])  # 清空推力指令缓存
+            print ("volt=", self.volt_records[-1], " thr=", self.cmd_records[-1])
 
-        # Calculate calibration parameters
-        if ((self.count_start_trigger_received >= 2) or 
-            (self.volt_records.size != 0 and self.volt_records[-1] < self.vbat_min)):
+        # 计算校准参数并保存数据
+        if ((self.count_start_trigger_received >= 2) or (self.volt_records.size != 0 and self.volt_records[-1] < self.vbat_min)):
             self.cal_and_save_data()
-            self.bat_sub.unregister()
+            self.bat_sub.unregister()  # 停止订阅电池状态信息
 
+    # 处理推力指令信息的回调函数
     def thrust_commands_cb(self, msg):
-
-        if (self.count_start_trigger_received == 0):
+        if (self.count_start_trigger_received == 0):  # 如果还未接收到触发信号，退出
             return
+        self.cmd_buf = np.append(self.cmd_buf, np.array([msg.thrust]), axis=0)  # 将推力指令信息加入缓存
+        print (msg.thrust)
 
-        self.cmd_buf = np.append(self.cmd_buf, np.array([msg.thrust]), axis=0)
-        print msg.thrust
-
+    # 处理起始触发信号的回调函数
     def start_trigger_cb(self, msg):
-
-        self.count_start_trigger_received += 1
+        self.count_start_trigger_received += 1  # 触发信号计数加一
         if(self.count_start_trigger_received == 1):
-            rospy.loginfo("Start recording.")
+            rospy.loginfo("Start recording.")  # 开始记录
         if(self.count_start_trigger_received > 1):
-            rospy.loginfo("Stop recording.")
+            rospy.loginfo("Stop recording.")  # 停止记录
 
+    # 计算校准参数并保存数据
     def cal_and_save_data(self):
-        rospy.loginfo("Data storing.")
+        rospy.loginfo("Data storing.")  # 记录数据
 
-        data_stack = np.vstack((self.cmd_records, self.volt_records))
-        data = [tuple(x) for x in data_stack.tolist()]
+        data_stack = np.vstack((self.cmd_records, self.volt_records))  # 堆叠推力指令和电压数据
+        data = [tuple(x) for x in data_stack.tolist()]  # 转换为元组列表
 
         rospack = rospkg.RosPack()
-        filedir = rospack.get_path(
-            'px4ctrl')+'/thrust_calibrate_scrips/data.csv'
-        f = open(filedir, 'a')
-        writer = csv.writer(f)
+        filedir = rospack.get_path('px4ctrl')+'/thrust_calibrate_scrips/data.csv'  # 获取数据存储路径
+        f = open(filedir, 'a')  # 打开CSV文件
+        writer = csv.writer(f)  # 创建CSV写入对象
         writer.writerow(((time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())),
-                         'mass(kg):', self.mass_kg, 'commands', 'voltage'))
-        writer.writerows(data)
-        f.close()
+                         'mass(kg):', self.mass_kg, 'commands', 'voltage'))  # 写入标题
+        writer.writerows(data)  # 写入数据
+        f.close()  # 关闭文件
 
-        rospy.loginfo("Stored to " + filedir)
-
+        rospy.loginfo("Stored to " + filedir)  # 记录数据存储位置
 
 if __name__ == '__main__':
 
     try:
-        rospy.init_node("thrust_calibration")
+        rospy.init_node("thrust_calibration")  # 初始化ROS节点
 
-        thrust_calibration = ThrustCalibration()
-        rospy.loginfo("Waiting for trigger.")
+        thrust_calibration = ThrustCalibration()  # 创建ThrustCalibration对象
+        rospy.loginfo("Waiting for trigger.")  # 等待触发信号
 
-        rospy.spin()
+        rospy.spin()  # 进入循环等待触发信号
 
     except rospy.ROSInterruptException:
         pass
